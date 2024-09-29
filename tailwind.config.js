7590d2ee95d5de5e351ec1ad65b85038a9934ce4
Dragoș Străinu <str.dr4605@gmail.com>
2024-09-29 12:54:23 +0300

    overflow scroll

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.html", "./src/**/*.vue", "./src/**/*.ts"],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
